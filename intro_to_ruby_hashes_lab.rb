e484ac6e6a3073c85f8e82832fb429268fc657d7
Kristin Kraemer <kristinhannahk@gmail.com>
2019-04-24 23:35:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,12 +30,12 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :names => {
-	      :reading_railroad => {},
+	      :reading_railroad=> {},
 	      :pennsylvania_railroad => {},
 	      :b_and_o_railroad => {},
 	      :shortline => {}
 	    },
-	    :rent_in_dollars => {
+	    :rent_in_dollars=> {
 	      :one_piece_owned => 25,
 	      :two_pieces_onwed => 50,
 	      :three_pieces_owned => 100,
